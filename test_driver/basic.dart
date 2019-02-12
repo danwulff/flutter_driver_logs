20c1a1c56a1f6ae33613cbbe93b68a115d7def19
Daniel Wulff <daniel.wulff@idexpertscorp.com>
2019-02-12 12:02:26 -0800

    add file for running basic test ye olde fashioned way

---
+++ test_driver/basic.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_app/src/app.dart';
+import 'package:flutter_driver/driver_extension.dart';
+
+void main() {
+  enableFlutterDriverExtension();
+
+  return runApp(App());
+}
